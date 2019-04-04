2faf9000648f6305b239528d6d784e57697e5e80
BhuvneshB <49224384+BhuvneshB@users.noreply.github.com>
2019-04-04 16:47:06 +0530

    Update Program_14a.py
    another way to plot tent map

--- Anaconda-files/Program_14a.py
+++ Anaconda-files/Program_14a.py
@@ -35,3 +35,26 @@ plt.ylabel('T(x)', fontsize=15)
 plt.tick_params(labelsize=15)
 
 plt.show()
+
+#another program for tent map
+'''import numpy as np
+import matplotlib.pyplot as plt
+xs=np.linspace(0,1,1001)
+r=2.0
+def f(x,r):
+    if x<=0.5:
+        return r*x
+    else:
+        return r-r*x
+
+Y=[f(x,r) for x in xs]
+Z=[f(f(x,r),r) for x in xs]
+
+plt.plot(xs,Y,'r')
+plt.plot(xs,Z,'b')
+plt.plot(xs,xs,'g')
+plt.xlabel('$x_n$')
+plt.ylabel('$x_{n+1}$')
+plt.title('Tent Map by Bhuvnesh Brawar')
+plt.show()'''
+
